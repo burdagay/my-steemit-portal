614ec6a4619c5920bd71f1ff04900bb62f47338f
Bryan Agustine C. Cabansay <burdagay@gmail.com>
2018-02-18 19:25:21 +0800

    Changed import statement*

--- app/bot.py
+++ app/bot.py
@@ -5,4 +5,4 @@ class FBChatbot:
     fb_api = Bot(os.environ['FB_ACCESS_TOKEN'])
 
     def send_text_message(sender, message):
-        fb_api.send_text_message(sender, "Echo: {}".format(message))
+        self.fb_api.send_text_message(sender, "Echo: {}".format(message))
