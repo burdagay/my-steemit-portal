56d1213414dda160d9f97310de8e195645f9f3d4
Bryan Agustine C. Cabansay <burdagay@gmail.com>
2018-02-18 17:57:41 +0800

    Modified Collec static files

--- my_steem_portal/settings.py
+++ my_steem_portal/settings.py
@@ -49,6 +49,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'my_steem_portal.urls'
@@ -127,3 +128,5 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
