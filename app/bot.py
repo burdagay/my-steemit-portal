251ece3d5aae6b7bf2466ac41e3c2034f3ba7d46
Bryan Agustine C. Cabansay <burdagay@gmail.com>
2018-02-18 19:26:51 +0800

    Testing final method

--- app/bot.py
+++ app/bot.py
@@ -4,5 +4,5 @@ import os
 class FBChatbot:
     fb_api = Bot(os.environ['FB_ACCESS_TOKEN'])
 
-    def send_text_message(sender, message):
+    def send_text_message(self, sender, message):
         self.fb_api.send_text_message(sender, "Echo: {}".format(message))
