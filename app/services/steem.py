1be623ca2d679bacb4addaa7777d83fe606ac3e5
Bryan Agustine C. Cabansay <burdagay@gmail.com>
2018-02-19 23:16:13 +0800

    Added payouts card

--- app/services/steem.py
+++ app/services/steem.py
@@ -1,13 +1,54 @@
 import json, math, requests
 from app import constants as CONST
 from steem import Steem
-from steem.converter import Converter
+from steem.steemd import Steemd
 from steem.account import Account
+from steem.blog import Blog
+from steem.converter import Converter
+from steem.instance import set_shared_steemd_instance
 
 class SteemHelper:
-    
-    s = Steem(nodes=[CONST.STEEMIT_API])
-    c = Converter(s)
+        
+    s = Steem(nodes=[CONST.STEEMIT_API, 'http://steemd.pevo.science'])
+    set_shared_steemd_instance(s)
+    c = Converter()    
+
+    def get_payout(self, username):
+        b = Blog(username)
+        posts = b.all()
+        total_pending = 0
+        total_payout = 0
+        total_cur_payout = 0
+        total_promoted = 0
+
+        for post in posts:
+            # Total pending value
+            pending_val = post['pending_payout_value'].amount
+            total_pending += pending_val            
+            # Total payout value
+            payout_val = post['total_payout_value'].amount
+            total_payout += payout_val
+            # Total curator payout value
+            cur_payout = post['curator_payout_value'].amount
+            total_cur_payout += cur_payout
+            # Total max accepted payout value
+            promoted = post['promoted'].amount
+            total_promoted += promoted
+        
+        total_pending = round(total_pending, 2)
+        total_payout = round(total_payout, 2)
+        total_cur_payout = round(total_cur_payout, 2)
+        total_promoted = round(total_promoted, 2)
+
+        result = {
+            'total_pending':"{} SP".format(total_pending),
+            'total_payout':"{} SP".format(total_payout),
+            'total_cur_payout':"{} SP".format(total_cur_payout),
+            'total_promoted':"{} SP".format(total_promoted)
+        }
+
+        return result
+        
 
     def get_account(self, username):
         a = self.s.get_account(username)
@@ -26,7 +67,7 @@ class SteemHelper:
     
     def get_steem_power(self, vests):
         vests = float(vests.replace('VESTS',''))
-        return round(self.c.vests_to_sp(vests),2)
+        return "{} SP".format(round(self.c.vests_to_sp(vests),2))
 
     def get_voting_power(self, vp):
         return "{}%".format(round(vp/100,2))
--- app/views.py
+++ app/views.py
@@ -9,6 +9,7 @@ def view_wallet(request):
     username = request.GET.get('username')
     user = steem.get_account(username)
     follow_cnt = steem.get_follow_count(username)
+    payout = steem.get_payout(username)
 
     context = {
         'username':username,
@@ -28,5 +29,9 @@ def view_wallet(request):
         'current_sp':steem.get_steem_power(user['vesting_shares']),
         'savings_steem':user['savings_sbd_balance'],
         'savings_sbd':user['savings_sbd_balance'],
+        'pending_payout':payout['total_pending'],
+        'total_payout':payout['total_payout'],
+        'curator_payout':payout['total_cur_payout'],
+        'total_promoted':payout['total_promoted'],
     }
     return render(request, 'app/wallet.html', context)
